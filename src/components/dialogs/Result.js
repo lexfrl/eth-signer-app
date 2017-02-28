b79c6166768a688013cf3a131a42604132cc0d4d
fro <frol.rage@gmail.com>
2017-02-28 13:19:23 +0300

    signed transaction QR code result implemented

--- src/components/dialogs/Result.js
+++ src/components/dialogs/Result.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, TextInput, View, ScrollView, Button, Picker } from 'react-native';
+import Svg, { Path, Rect, Circle } from 'react-native-svg';
+import qr from 'javascript-qrcode';
 
 type Transaction = Object;
 
@@ -7,16 +9,25 @@ export default class Result extends Component {
 
   props: {
     transaction: Transaction,
+    size: number,
   };
 
   render() {
     const { signedTx, onSubmit } = this.props;
+    const code = new qr.QrCode(signedTx);
+    const qzone = 5;
+    const msize = 2;
+    const { d, width, height } = this.genSvgPath(code.getData(), msize, qzone);
     return (
-      <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between', alignItems: 'stretch', flexBasis: 800, width: 250 }}>
-        <ScrollView style={ styles.transactionContainer } >
-          <Text style={styles.title}>{ signedTx }</Text>
-        </ScrollView>
+      <View style={{ }}>
+        <Text style={styles.title}>Signed transaction</Text>
         <View style={ { flexShrink: 1, marginTop: 5, marginBottom: 10, borderBottomWidth: 0.5, borderBottomColor: '#000' } } />
+        <View style={ { flexShrink: 1, flexDirection: 'row', justifyContent: 'center'} }>
+        <Svg height="200" width="200" viewBox={'0 0 ' + (width + 2 * qzone * msize) + ' ' + (height + 2 * qzone * msize) + ''}>
+          <Path d={d} stroke="black" />
+        </Svg>
+        </View>
+
         <View style={ { flexShrink: 1, flexDirection: 'row',  height: 40, justifyContent: 'center'} }>
           <Button
             style={{}}
@@ -26,7 +37,27 @@ export default class Result extends Component {
       </View>
     );
   }
+
+  genSvgPath(code, size = 3, quietZoneSize = 4) {
+    const width = code.length * size;
+    const height = code.length * size;
+    let r, c;
+    let d = '';
+
+    for (r = 0; r < code.length; r += 1) {
+      for (c = 0; c < code[r].length; c += 1) {
+        if (code[r][c] === 1) {
+          d += 'M' + ((c + quietZoneSize) * size) + ' ' + ((r + quietZoneSize) * size) + 'h' + size + 'v' + size + 'h-' + size + 'z';
+        }
+      }
+    }
+    return { d, width, height }
+  }
 }
+
+Result.defaultProps = {
+  size: 200,
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -40,8 +71,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   transactionContainer: {
-    flex: 1,
-    flexGrow: 1,
+    height: 200,
   },
   transactionView: {
     flex: 1,
